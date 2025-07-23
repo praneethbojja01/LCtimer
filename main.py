43c6475c4b06256b56bf9179caea7c381cd50bb9
praneethbojja01 <praneethbojja2313@gmail.com>
2025-07-22 21:51:21 -0700

    Added logging to CSV and additional CLI features

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import typer
 from tracker.timer import startTimer, endTimer
+from rich import print
 
 app = typer.Typer()
 
 @app.command()
-def start(id: str=typer.Option(..., "--id", "-id", help="problem ID and name for better tracking")):
+def start(id: str=typer.Option(
+    ...,
+    "--id", "-i",
+    help="problem ID and name for better tracking",
+    prompt="Please add a problemID for this(LCID_Problem_name)")):
+
+    #print(f"[green]Problem id received {id}[/green]")
     startTimer(id)
 
 @app.command()
-def end():
-    endTimer()
+def end(solved: str=typer.Option(..., "--solved", "-s", help= "Was able to solve the question(?)", prompt="Please add this value, Solved(?)"),
+        difficulty: str=typer.Option(..., "--difficulty", "-d", help= "difficulty level of the problem. Easy/Medium/Hard", prompt="Please add this value, Difficulty"),
+        tags: str= typer.Option(..., "--tags", "-t", help= "tags related to the problem", prompt="Please add this value, tags"),
+        notes: str= typer.Option(..., "--notes", "-n", help= "short notes to remember about the problem", prompt="Please add this value, Notes")):
+    endTimer(solved, difficulty, tags, notes)
 
 if __name__=="__main__":
     app()
--- tracker/timer.py
+++ tracker/timer.py
@@ -1,34 +1,36 @@
 import os
 from rich import print
 import json
+from tracker import logger
 from datetime import datetime
 
-TIMER_TEMP= ".lc_tmptimer.json"
+TIMER_TMP_FILE= ".lc_tmptimer.json"
 
 def startTimer(problemID):
-    if os.path.exists(TIMER_TEMP):
-        with open(TIMER_TEMP, 'r') as f:
+    if os.path.exists(TIMER_TMP_FILE):
+        with open(TIMER_TMP_FILE, 'r') as f:
             data = json.load(f)
         print(f"[red]A timer is already running for the problem: {data['problemID']}[/red]")
         return
     data = {
         "problemID": problemID,
-        "startTime": datetime.now().isoformat()
+        "start_time": datetime.now().isoformat()
     }
-    with open(TIMER_TEMP, 'w') as f:
+    with open(TIMER_TMP_FILE, 'w') as f:
         json.dump(data,f,indent=4)
-    print(f"[green]Timer has been started for {problemID} at {datetime.fromisoformat(data['startTime'])}[/green]")
+    print(f"[green]Timer has been started for {problemID} at {datetime.fromisoformat(data['start_time'])}[/green]")
     return
 
-def endTimer():
-    if not os.path.exists(TIMER_TEMP):
+def endTimer(solved, difficulty, tags, notes):
+    if not os.path.exists(TIMER_TMP_FILE):
         print(f"[red]There is no active timer running for any problem. Start a timer first before ending.[/red]")
         return
-    with open(TIMER_TEMP, 'r') as f:
+    with open(TIMER_TMP_FILE, 'r') as f:
         data = json.load(f)
-    startTime = datetime.fromisoformat(data['startTime'])
-    endTime = datetime.now()
-    duration = endTime - startTime
-    os.remove(TIMER_TEMP)
-    print(f"[yellow]Timer has been ended for {data['problemID']}, here are some stats,\nStart Time: {startTime}\nEnd Time: {endTime}\nDuration: {duration}[/yellow]")
+    start_time = datetime.fromisoformat(data['start_time'])
+    end_time = datetime.now()
+    duration = end_time - start_time
+    os.remove(TIMER_TMP_FILE)
+    logger.log_to_csv(data['problemID'], start_time.strftime("%H:%M:%S"), end_time.strftime("%H:%M:%S"), duration, solved, difficulty, tags, notes)
+    print(f"[yellow]Timer has been ended for {data['problemID']}, here are some stats,\nStart Time: {start_time.strftime("%H:%M:%S")}\nEnd Time: {end_time.strftime("%H:%M:%S")}\nDuration: {duration}[/yellow]")
     return
